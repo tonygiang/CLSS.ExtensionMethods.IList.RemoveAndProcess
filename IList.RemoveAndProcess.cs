931037f7b235d1aac9250a51650663419786fa3e
tonygiang <tony_s_giang@yahoo.com>
2022-08-31 01:07:09 +0700

    Project push.

---
+++ IList.RemoveAndProcess.cs
@@ -0,0 +1,89 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IListRemoveAndProcess
+  {
+    /// <inheritdoc cref="RemoveAndProcess{T, TElement, TR}(T, int, Func{TElement, TR})"/>
+    public static T RemoveAndProcess<T, TElement>(this T source,
+      int index,
+      Action<TElement> processingAction)
+      where T : IList<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[index];
+      source.RemoveAt(index);
+      processingAction(element);
+      return source;
+    }
+
+    /// <inheritdoc cref="RemoveAndProcess{T, TElement, TR}(T, int, Func{TElement, TR})"/>
+    public static IList<TElement> RemoveAndProcess<TElement>(
+      this IList<TElement> source,
+      int index,
+      Action<TElement> processingAction)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[index];
+      source.RemoveAt(index);
+      processingAction(element);
+      return source;
+    }
+
+    /// <summary>
+    /// Takes the element at the specified index number and passes it to the
+    /// specified delegate. Removes that element from the source collection
+    /// afterward.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IList{T}"/> to remove element
+    /// from.</typeparam>
+    /// <typeparam name="TElement"><inheritdoc cref="IList{T}"
+    /// path="/typeparam[@name='T']"/></typeparam>
+    /// <typeparam name="TResult">The return type of
+    /// <paramref name="processingAction"/>.</typeparam>
+    /// <param name="source">The <see cref="IList{T}"/> to remove element.
+    /// </param>
+    /// <param name="index"><inheritdoc cref="IList{T}.RemoveAt(int)"
+    /// path="/param[@name='index']"/></param>
+    /// <param name="processingAction">The action that will be executed with the
+    /// removed element as its argument.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="processingAction"/> is null.</exception>
+    public static T RemoveAndProcess<T, TElement, TResult>(this T source,
+      int index,
+      Func<TElement, TResult> processingAction)
+      where T : IList<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[index];
+      source.RemoveAt(index);
+      processingAction(element);
+      return source;
+    }
+
+    /// <inheritdoc cref="RemoveAndProcess{T, TElement, TR}(T, int, Func{TElement, TR})"/>
+    public static IList<TElement> RemoveAndProcess<TElement, TResult>(
+      this IList<TElement> source,
+      int index,
+      Func<TElement, TResult> processingAction)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (processingAction == null)
+        throw new ArgumentNullException("processingAction");
+      var element = source[index];
+      source.RemoveAt(index);
+      processingAction(element);
+      return source;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+﻿# CLSS.ExtensionMethods.IList.RemoveAndProcess
+
+### Problem
+
+It's common to find situations where removing an object from a collection means you also want to do something with it, such as disposing of it. In a barebone .NET application, you can count on the .NET Garbage Collector to clean up the removed object, but in many domains, disposing of an unused object requires an additional manual call, as the framework still keep references to it. These 2 operations are each done in a separate line of code and therefore not very friendly to a functional syntax.
+
+```csharp
+DisposeObject(Objects[4]);
+Objects.RemoveAt(4);
+```
+
+### Solution
+
+`RemoveAndProcess` is an extension method for all `IList<T>` types that combines these 2 steps into 1 method.
+
+```csharp
+using CLSS;
+
+Objects.RemoveAndProcess(4, DisposeObject);
+// using a lambda expression
+Objects.RemoveAndProcess(4, obj => { obj.Disable(); obj.Hide(); });
+```
+
+`RemoveAndProcess` takes the element it finds at that index number specified by the first argument, removes the element from the source collection and passes that element to the delegate in the second argument.
+
+Non-void methods are also accepted as the second argument.
+
+```csharp
+using CLSS;
+
+bool TryDispose(ResourceHandler handler) { ... };
+
+var FileResources = new List<ResourceHandler>();
+FileResources.RemoveAndProcess(4, TryDispose);
+```
+
+`RemoveAndProcess` returns the source `IList<T>` to be friendly to a functional-style call chain. The exact return type will be determined by the invocation syntax of `RemoveAndProcess`. With an implicit type invocation, it returns an `IList<T>`. With an explicit type invocation, it returns the original collection type.
+
+```csharp
+using CLSS;
+
+var numbers = new int[] { 0, 1, 2, 3, 4 };
+numbers.RemoveAndProcess(2, str => { ... }); // returns IList<int>
+numbers.RemoveAndProcess<int[], int>(2, str => { ... }); // returns int[]
+```
+
+**Note**: `RemoveAndProcess` works on all types implementing the [`IList<T>`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ilist-1) interface, *including raw C# array*.
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
